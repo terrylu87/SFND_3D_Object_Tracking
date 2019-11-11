cd45e1f5af617a9bf8fa4729a28934a8fa131516
TerryLu <terry.lu@aptiv.com>
2019-11-11 14:02:53 +0800

    calculate TCC from lidar points, simply discard the unresonable result which will give an unresonable acceleration value

--- src/camFusion_Student.cpp
+++ src/camFusion_Student.cpp
@@ -150,7 +150,48 @@ void computeTTCCamera(std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPo
 void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
                      std::vector<LidarPoint> &lidarPointsCurr, double frameRate, double &TTC)
 {
-    // ...
+    // auxiliary variables
+    static double previous_TTC = -1;
+    static double previous_speed = 0;
+    const double max_acceleration = 9.8; // set max acceleration to g = 9.8
+    double dT = 1/frameRate;        // time between two measurements in seconds
+    double laneWidth = 4.0; // assumed width of the ego lane
+
+    // find closest distance to Lidar points within ego lane
+    double minXPrev = 1e9, minXCurr = 1e9;
+    for (auto it = lidarPointsPrev.begin(); it != lidarPointsPrev.end(); ++it)
+    {
+        if(it->y > -laneWidth/2 && it->y < laneWidth/2){
+            minXPrev = minXPrev > it->x ? it->x : minXPrev;
+        }
+    }
+
+    for (auto it = lidarPointsCurr.begin(); it != lidarPointsCurr.end(); ++it)
+    {
+        if(it->y > -laneWidth/2 && it->y < laneWidth/2){
+            minXCurr = minXCurr > it->x ? it->x : minXCurr;
+        }
+    }
+
+    // compute TTC from both measurements
+    TTC = minXCurr * dT / (minXPrev - minXCurr);
+    //cout << "TTC == " << TTC << endl;
+    if(previous_TTC  > 0){
+        double speed = (minXPrev - minXCurr) / dT;
+        //cout << "current speed == " << speed << endl;
+        double a = (previous_speed - speed) / dT;
+        //cout << "a == " << a << endl;
+        if(abs(a) < max_acceleration){
+            previous_speed = speed;
+        }else{
+            //cout << "discard this frame" << endl;
+            TTC = previous_TTC - dT;
+        }
+        previous_TTC = TTC;
+    }else{
+        previous_TTC = TTC;
+        previous_speed = (minXPrev - minXCurr) / dT;
+    }
 }
 
 
